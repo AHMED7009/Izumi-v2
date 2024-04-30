40cfd98a95c09f3da483c9682ef7ac52f95952ee
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-04-30 21:51:35 +0300

    Create modapk.js

---
+++ plugins/modapk.js
@@ -0,0 +1,33 @@
+import { download } from 'aptoide-scraper';
+
+let handler = async (m, { conn, usedPrefix: prefix, command, text }) => {
+  try {
+    if (command === 'modapk') {
+      if (!text) throw `*[❗] ارسل اسم التطبيق لتحميله*`;
+
+      await conn.reply(m.chat, global.wait, m);
+      let data = await download(text);
+
+      if (data.size.replace(' MB', '') > 20000) {
+        return await conn.sendMessage(m.chat, { text: '*[😩] حجم التطبيق كبير جدا.*' }, { quoted: m });
+      }
+
+      if (data.size.includes('GB')) {
+        return await conn.sendMessage(m.chat, { text: '*[😩] حجم التطبيق كبير جدا.*' }, { quoted: m });
+      }
+
+      await conn.sendMessage(
+        m.chat,
+        { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null },
+        { quoted: m }
+      )
+    }
+  } catch {
+    throw `*[❗] تعذر تحميل التطبيق.*`;
+  }
+};
+
+handler.help = ['modapk']
+handler.tags = ['downloader']
+handler.command = /^modapk$/i;
+export default handler;
